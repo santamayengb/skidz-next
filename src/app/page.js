7eb492e29303af79c6567eff8aafb90d6a693849
santamayenb <19mca8232@cuchd.in>
2024-10-06 21:01:48 +0530

    "Added two new list items to the ordered list in Home component"

--- src/app/page.js
+++ src/app/page.js
@@ -21,6 +21,8 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>testing changes</li>
+          <li>testing changes</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
