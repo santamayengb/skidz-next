e734eb2edb97fbc45baebcc583a561f382627c9c
santamayenb <19mca8232@cuchd.in>
2024-10-06 21:14:52 +0530

    new build from local

--- src/app/page.js
+++ src/app/page.js
@@ -21,8 +21,9 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
-          <li>testing changes</li>
-          <li>testing changes</li>
+          <li>testing changes 1</li>
+          <li>testing changes 2</li>
+          <li>testing changes 3</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
