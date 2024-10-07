e51b9f962c1701f3f6ed5c5bca6ddc36deb5b531
santamayenb <19mca8232@cuchd.in>
2024-10-07 14:28:32 +0530

    "Added a new list item 'testing changes 5' to the ordered list in Home component."

--- src/app/page.js
+++ src/app/page.js
@@ -25,6 +25,7 @@ export default function Home() {
           <li>testing changes 2</li>
           <li>testing changes 3</li>
           <li>testing changes 4</li>
+          <li>testing changes 5</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
